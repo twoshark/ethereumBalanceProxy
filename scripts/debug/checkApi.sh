21a5db3803beebe7049e7e4e956a7485fc302d1d
Tushar Shah <twoshark@gmail.com>
2023-02-09 10:13:41 -0700

    Quick manual verification script

---
+++ scripts/debug/checkApi.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+URL="${URL:=http://localhost:8080}"
+
+curl "${URL}"/ethereum/balance/0x74630370197b4c4795bFEeF6645ee14F8cf8997D
+
+curl "${URL}"/ethereum/balance/0x74630370197b4c4795bFEeF6645ee14F8cf8997D/block/16363048
