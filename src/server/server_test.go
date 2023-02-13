b95d3655c8ff42a18c1aba5a89f7aadc5535e05d
Tushar Shah <twoshark@gmail.com>
2023-02-12 21:53:08 -0700

    Metrics Tests added to server tests check for existence of custom metrics created by the app and that general go and echo statistics show up

--- src/server/server_test.go
+++ src/server/server_test.go
@@ -2,24 +2,53 @@ package server
 
 import (
 	"context"
+	"io"
 	"net/http"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
-
-	"github.com/twoshark/balanceproxy/src/common"
 
 	"github.com/ethereum/go-ethereum/ethclient"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+	"github.com/twoshark/balanceproxy/src/common"
 )
+
+func RunSuiteWithServer(t *testing.T, testSuite suite.TestingSuite) {
+	port := viper.GetString("PORT")
+	ready := make(chan bool)
+	go Start(common.AppConfiguration{
+		ListenPort: port,
+		Endpoints: []string{
+			"https://eth.getblock.io/b33bc13b-2d6b-4112-bd43-d93bb7cf842a/mainnet/",
+			"https://fittest-falling-smoke.discover.quiknode.pro/",
+		},
+	}, ready)
+	for {
+		select {
+		case <-ready:
+			close(ready)
+			suite.Run(t, testSuite)
+			return
+		default:
+			log.Print("Waiting for Server Startup...")
+			time.Sleep(10 * time.Second)
+		}
+	}
+}
 
 type ServerTestSuite struct {
 	suite.Suite
 	endpoints []string
 	port      string
+}
+
+func TestBalanceProxyServerTestSuite(t *testing.T) {
+	common.CobraInit()
+	RunSuiteWithServer(t, new(ServerTestSuite))
 }
 
 func (suite *ServerTestSuite) SetupSuite() {
@@ -37,6 +66,24 @@ func (suite *ServerTestSuite) TearDownSuite() {
 
 func (suite *ServerTestSuite) TestRootHandler() {
 	resp, err := http.Get("http://localhost:" + suite.port)
+	assert.Equal(suite.T(), nil, err)
+	assert.NotEqual(suite.T(), nil, resp)
+	if resp != nil {
+		assert.Equal(suite.T(), 200, resp.StatusCode)
+	}
+}
+
+func (suite *ServerTestSuite) TestLiveHandler() {
+	resp, err := http.Get("http://localhost:" + suite.port + "/live")
+	assert.Equal(suite.T(), nil, err)
+	assert.NotEqual(suite.T(), nil, resp)
+	if resp != nil {
+		assert.Equal(suite.T(), 200, resp.StatusCode)
+	}
+}
+
+func (suite *ServerTestSuite) TestReadyHandler() {
+	resp, err := http.Get("http://localhost:" + suite.port + "/ready")
 	assert.Equal(suite.T(), nil, err)
 	assert.NotEqual(suite.T(), nil, resp)
 	if resp != nil {
@@ -70,26 +117,30 @@ func (suite *ServerTestSuite) TestBalanceHandler() {
 	}
 }
 
-func TestBalanceProxyServerTestSuite(t *testing.T) {
-	common.CobraInit()
-	port := viper.GetString("PORT")
-	ready := make(chan bool)
-	go Start(common.AppConfiguration{
-		ListenPort: port,
-		Endpoints: []string{
-			"https://eth.getblock.io/b33bc13b-2d6b-4112-bd43-d93bb7cf842a/mainnet/",
-			"https://fittest-falling-smoke.discover.quiknode.pro/",
-		},
-	}, ready)
-	for {
-		select {
-		case <-ready:
-			close(ready)
-			suite.Run(t, new(ServerTestSuite))
-			return
-		default:
-			log.Print("Waiting for Server Startup...")
-			time.Sleep(10 * time.Second)
-		}
+func (suite *ServerTestSuite) TestMetricHandler() {
+	resp, err := http.Get("http://localhost:" + suite.port + "/metrics")
+	assert.NoError(suite.T(), err)
+	assert.NotNil(suite.T(), resp)
+	if err != nil {
+		return
 	}
+	defer resp.Body.Close()
+	responseData, err := io.ReadAll(resp.Body)
+	assert.NoError(suite.T(), err)
+	assert.Greater(suite.T(), len(responseData), 0)
+	response := string(responseData)
+
+	// Custom Metrics
+	assert.True(suite.T(), strings.Contains(response, "startup_time_ms"))
+	assert.True(suite.T(), strings.Contains(response, "upstreams_configured"))
+	assert.True(suite.T(), strings.Contains(response, "upstreams_healthy"))
+	assert.True(suite.T(), strings.Contains(response, "latency_eth_syncing"))
+	assert.True(suite.T(), strings.Contains(response, "latency_eth_get_block_number"))
+	assert.True(suite.T(), strings.Contains(response, "latency_eth_get_balance"))
+
+	// Go Metrics
+	assert.True(suite.T(), strings.Contains(response, "go_"))
+
+	// Echo Metrics
+	assert.True(suite.T(), strings.Contains(response, "echo"))
 }
