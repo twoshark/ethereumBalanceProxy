7413a3f0a891b3456574e7fc8b830a4d417e9eea
Tushar Shah <twoshark@gmail.com>
2023-02-13 15:18:51 -0700

    Ops Commands - call: call eth rpc methods in the same fashion as the server - healthCheck: run manager's healthCheck against an endpoint

--- src/cmd/ops.go
+++ src/cmd/ops.go
@@ -5,16 +5,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var endpoint *string
+
 // opsCmd represents the ops command.
 var opsCmd = &cobra.Command{
 	Use:   "ops",
-	Short: "ops commands for the ethereum balance proxy",
+	Short: "parent call for ops commands for the ethereum balance proxy.",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Print("ops called")
+		log.Print("no-op")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(opsCmd)
+	endpoint = opsCmd.PersistentFlags().String("endpoint", "", "Ethereum JSON RPC Endpoint")
 }
