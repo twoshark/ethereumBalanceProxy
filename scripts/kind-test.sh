e16fba3a6b96aeec970fb3fecb24822b8643650e
Tushar Shah <twoshark@gmail.com>
2023-02-15 23:20:08 -0700

    hash for image tag

--- scripts/kind-test.sh
+++ scripts/kind-test.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-echo "TBD"
+podname=$(kubectl get po -o name -n balance-proxy-test --context kind-balance-proxy-cluster)
+kubectl port-forward --context kind-balance-proxy-cluster "${podname}" 8080:8080 &
+echo "Wait for forwarding.."
+sleep 20
+sh scripts/debug/checkApi.sh
